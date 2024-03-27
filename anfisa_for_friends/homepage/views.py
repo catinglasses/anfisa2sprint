0d15859fef91e17e02779dddd0c65aa5e49b9e85
Saveliy Efimov <savva.cat@mail.ru>
2024-03-27 13:52:47 +0300

    new html + edit view-func

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -6,10 +6,13 @@ def index(request):
     template = 'homepage/index.html'
 
     ice_cream_list = IceCream.objects.values(
-        'id', 'title', 'description'
+        'id', 'title', 'price', 'description'
     ).filter(
-        is_published=True, is_on_main=True
+        is_published=True,
+        is_on_main=True,
+        category__is_published=True
     )
+
     context = {
         'ice_cream_list': ice_cream_list,
     }
--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -8,7 +8,7 @@ class Category(PublishedModel):
     slug = models.SlugField(max_length=64, unique=True, verbose_name='Слаг')
     output_order = models.PositiveSmallIntegerField(
         default=100,
-            
+        verbose_name='Порядок отображения'
         )
 
     class Meta:
@@ -47,6 +47,8 @@ class Wrapper(PublishedModel):
 class IceCream(PublishedModel):
     title = models.CharField(max_length=256, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
+    price = models.DecimalField(max_digits=5, decimal_places=2, default=0, verbose_name='Цена')
+    output_order = models.PositiveSmallIntegerField(default=100, verbose_name='Порядок отображения')
     wrapper = models.OneToOneField(
         Wrapper,
         on_delete=models.SET_NULL,
@@ -67,6 +69,7 @@ class IceCream(PublishedModel):
     class Meta:
         verbose_name = 'мороженое'
         verbose_name_plural = 'Мороженое'
+        ordering = ('output_order', 'title')
 
     def __str__(self):
         return self.title
